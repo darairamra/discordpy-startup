cb2d60e2bb275ad41b6856736ccef81c0ed2b31c
darairamra <69851276+darairamra@users.noreply.github.com>
2020-08-18 22:26:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,19 @@
 from discord.ext import commands
 import os
 import traceback
+import discord
 
+
+client = discord.Client()
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("test"):
+       if client.user != message.author:
+        msg = "失敗" + massage.author.name
+        await message.channel.send(msg)
 
 
 @bot.event
@@ -11,7 +21,6 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
 
 @bot.command()
 async def ping(ctx):
@@ -23,3 +32,4 @@ async def dii(ctx):
 
 
 bot.run(token)
+client.run(token)
