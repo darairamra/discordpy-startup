5a35fe89eeaeab81e9e3f5c491a39c55159d3af6
darairamra <69851276+darairamra@users.noreply.github.com>
2020-08-19 21:06:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,10 +12,15 @@ import random
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
+@client.event
+async def on_ready():
+    channel = guild.get_channel(737270360277254)
+    msg = "起動完了"
+    await client.send_message(channel,msg)
+            
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author.bot:
         return
     
     if message.content.startswith('/dii'):
