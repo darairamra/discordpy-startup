7a1184435f750b8e87da42187c9cdd3e4471021f
darairamra <69851276+darairamra@users.noreply.github.com>
2020-08-19 20:10:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,41 +7,44 @@ from urllib import parse, request
 import re
 import datetime
 import asyncio
+from discord.ext.commands import Bot
+import platform
+import random
 
-client = discord.Client()
-bot = commands.Bot(command_prefix='/')
+
+client = Bot(command_prefix="/", pm_help = False)
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
+@client.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@bot.command()
+@client.command()
 async def ping(ctx):
     await ctx.send('pong')
     
-@bot.command()
+@client.command()
 async def dii(ctx):
     await ctx.send("バカ")
     
     
-@bot.command()
+@client.command()
 async def sum(ctx, numOne: int, numTwo: int):
     await ctx.send(numOne + numTwo)
 
  
-@bot.event
+@client.event
 async def on_voice_state_update(member, before, after): 
     if member.name == "Dii" or "だらい":
         alert_channel = '737270360277254164'
         voice_channel = '250171026988728320'
-        channel = bot.get_channel(alert_channel)
+        channel = client.get_channel(alert_channel)
         if after.channel.id == voice_channel: 
             msg = f'{member.name} さんが {after.channel.name} に入室しました！'
             await channel.send(msg)
     
     
-bot.run(token)
+client.run(token)
