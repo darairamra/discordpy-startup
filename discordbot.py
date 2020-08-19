e16a96857804538b127ab42246210e004d36c95b
darairamra <69851276+darairamra@users.noreply.github.com>
2020-08-20 02:33:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,12 +12,12 @@ import random
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 TEST_ID = 737270360277254
-test_channel = client.get_channel(TEST_ID)
+
 
 @client.event
 async def on_ready():
-    
-    await test_channel.send("起動")
+   test_channel = client.get_channel(TEST_ID)
+　　await test_channel.send("起動")
             
 @client.event
 async def on_message(message):
@@ -27,7 +27,5 @@ async def on_message(message):
     if message.content == '/dii':
         await message.channel.send("バカ")
     
-    if message.content == '/test':
-        await test_channel.send("バカ")
         
 client.run(token)
