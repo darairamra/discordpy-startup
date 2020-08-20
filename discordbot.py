14f865416f92d87804272c011d5e486b5224bb3a
darairamra <69851276+darairamra@users.noreply.github.com>
2020-08-20 20:31:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,20 +5,17 @@ import discord
 from urllib import parse, request
 import re
 import datetime
-import nest_asyncio
-import platform
-import random
-nest_asyncio.apply()
+
 
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 TEST_ID = 737270360277254164
 
 
-@client.event
-async def on_ready():
-   channel1 = client.get_channel(TEST_ID)
-   await channel1.send("起動")
+#@client.event
+#async def on_ready():
+#   channel1 = client.get_channel(TEST_ID)
+#   await channel1.send("起動")
             
 @client.event
 async def on_message(message):
