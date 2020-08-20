6c451849707b3835e10c97fd8d7e1a8bea0b1b04
darairamra <69851276+darairamra@users.noreply.github.com>
2020-08-20 20:34:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,10 +12,10 @@ token = os.environ['DISCORD_BOT_TOKEN']
 TEST_ID = 737270360277254164
 
 
-#@client.event
-#async def on_ready():
-#   channel1 = client.get_channel(TEST_ID)
-#   await channel1.send("起動")
+@client.event
+async def on_ready():
+   channel1 = client.get_channel(TEST_ID)
+   await channel1.send("起動")
             
 @client.event
 async def on_message(message):
